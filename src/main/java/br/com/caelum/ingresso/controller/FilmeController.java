53c8ab0ba7dc21bb5fca4975c8f9ef00b211a04e
anderson <andersoncerberos@gamil.com>
2020-01-14 22:06:35 -0300

    ex44

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -29,23 +31,29 @@ public class FilmeController {
 
 	@Autowired
 	private FilmeDao filmeDao;
-	
+
 	@Autowired
 	private SessaoDao sessaoDao;
 
-	
+	@Autowired
+	private OmdbClient client;
+
 	@GetMapping("/filme/{id}/detalhe")
 	public ModelAndView detalhes(@PathVariable("id") Integer id) {
-		
+
 		ModelAndView modelAndView = new ModelAndView("/filme/detalhe");
-		Filme filme =  filmeDao.findOne(id);
+		Filme filme = filmeDao.findOne(id);
 		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
-		
+
+		Optional<DetalhesDoFilme> detalhedofilme = client.request(filme);
+
 		modelAndView.addObject("sessoes", sessoes);
-		
+		modelAndView.addObject("detalhes", detalhedofilme.orElse(new DetalhesDoFilme()));
+
 		return modelAndView;
-		
+
 	}
+
 	@GetMapping("/filme/em-cartaz")
 	public ModelAndView emCartaz() {
 
